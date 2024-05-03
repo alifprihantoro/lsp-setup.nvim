dbfb5e79170d4d5445dc55a21e1ff7e36d679e3d
alifprihantoro <alifprihantoro@gmail.com>
2024-05-04 05:52:31 +0700

    fix: astro lsp

--- after/plugin/servers/astro.lua
+++ after/plugin/servers/astro.lua
@@ -2,7 +2,7 @@ local lsp = require 'lspconfig'
 lsp.astro.setup {
   init_options = {
     typescript = {
-      tsdk = vim.fs.normalize '/data/data/com.termux/files/usr/lib/node_modules/typescript/lib',
+      tsdk = vim.fs.normalize '$HOME/.pnpm/global/5/node_modules/typescript/lib/',
     },
   },
 }
